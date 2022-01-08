64432a2228881c63627d961d285e06adab517747
Stefanescu Radu <radu98fm@gmail.com>
2022-01-08 18:09:10 +0200

    save to json wtih cart

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,5 +1,8 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.service.Cart;
+import com.codecool.shop.service.CartItem;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -14,23 +17,44 @@ import java.nio.file.Paths;
 
 @WebServlet(urlPatterns = {"/checkout"})
 public class CheckoutController extends HttpServlet {
+    private final Cart shoppingCart = Cart.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Reader in = new BufferedReader(new InputStreamReader((req.getInputStream())));
-
         StringBuilder sb = new StringBuilder();
         for (int c; (c = in.read()) >= 0; )
             sb.append((char) c);
-        String str = sb.toString();
+        String incomeJson = sb.toString();
         JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = (JsonObject) jsonParser.parse(str);
-        Files.write(Paths.get(jsonObject.get("id")+".json"), str.getBytes());
+        JsonObject jsonToSave = (JsonObject) jsonParser.parse(incomeJson);
+
+        JsonObject cartJson = getCartJson();
+
+        jsonToSave.add("cart", cartJson);
+        Files.write(Paths.get(jsonToSave.get("id") + ".json"), jsonToSave.toString().getBytes());
+    }
+
+    private JsonObject getCartJson() {
+        JsonObject cartJson = new JsonObject();
+        JsonArray cartItemsArray = new JsonArray();
+
+
+        for (CartItem item : shoppingCart.getCartItems()) {
+            JsonObject cartItem = new JsonObject();
+            System.out.println("Adding "+ item.getProduct().getName());
+            cartItem.addProperty("name", item.getProduct().getName());
+            cartItem.addProperty("quantity", item.getQuantity());
+            cartItem.addProperty("price",item.getProduct().getPrice());
+            cartItem.addProperty("subtotal",item.getSubTotalPrice());
+            cartItemsArray.add(cartItem);
+        }
+        cartJson.add("items", cartItemsArray);
+        cartJson.addProperty("totalPrice",shoppingCart.getTotalPrice());
+        return cartJson;
     }
 
 }
--- src/main/java/com/codecool/shop/controller/PaymentPage.java
+++ src/main/java/com/codecool/shop/controller/PaymentPage.java
@@ -1,5 +1,17 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.service.Cart;
+import com.codecool.shop.service.ProductService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,15 +21,26 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/payment"})
 public class PaymentPage extends HttpServlet {
+    private final Cart shoppingCart = Cart.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        System.out.println("payment route doGet");
 
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore);
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("products", shoppingCart.getCartItems());
+
+        engine.process("product/payment.html", context, resp.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-
     }
-
 }
