61a33feeecca62a2a995958d5a599741fae27339
Stefanescu Radu <radu98fm@gmail.com>
2022-01-08 12:46:09 +0200

    added side menu functionality plus css refactored

--- src/main/webapp/static/JS/sidemenu.js
+++ src/main/webapp/static/JS/sidemenu.js
@@ -1,14 +1,17 @@
+init()
+
+function init(){
+    document.getElementById("sidenavTrigger").addEventListener("mouseenter", openNav);
+}
+
 function openNav() {
     document.getElementById("sideNavMenu").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
 }
 
 function closeNav() {
     document.getElementById("sideNavMenu").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-}
+
 
 console.log("Testing!");
 openNav();
